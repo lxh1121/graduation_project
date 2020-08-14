720f5c16c623f71e3a2954de653fcddaf6f4c09c
卢小灰 <44663351+lxh1121@users.noreply.github.com>
2020-08-14 20:24:22 +0800

    Update settings.py

--- lxh/lxh/settings.py
+++ lxh/lxh/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@gck@(nfs6rgg$l2utk3=@fs@gm_iu*iixv-$)r7fo$hh%$^1e'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -71,8 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lxh.wsgi.application'
-
+WSGI_APPLICATION = 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
